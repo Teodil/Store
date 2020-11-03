6efe6f223f86575acbe94aeb868f8e7fda063b45
Babinceva <47178606+Teodil@users.noreply.github.com>
2020-11-03 13:53:05 +1000

    1 lesson

---
+++ infrostructure/Store.Memory/BookRepresetory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Memory
+{
+    public class BookRepresetory : IBookRepresetory
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1,"Art of Programming"),
+            new Book(2,"Refactoring"),
+            new Book(3,"C Programming Lanuage"),
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepresetory bookRepresetory;
+        public SearchController(IBookRepresetory bookRepresetory)
+        {
+            this.bookRepresetory = bookRepresetory;
+        }
+        public IActionResult Index(string query)
+        {
+            var books = bookRepresetory.GetAllByTitle(query);
+            return View(books);
+        }
+    }
+}
